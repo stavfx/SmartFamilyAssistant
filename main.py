e76bb5db1e34e3e163bbf61a12fb9bdb02faca63
Rich Fromm <rich@locationlabs.com>
2018-10-04 18:09:18 -0700

    massive change, actual parsing
    assuming get_json() really returns a dict, not json
    maybe I should have fully tested that first
    
    but when trying to cal loads(request_json), we got:
    the JSON object must be str, bytes or bytearray, not dict

--- main.py
+++ main.py
@@ -18,6 +18,56 @@ def rich_test(request):
     pass
 
 
+def get_location(name):
+    return f"Get the location of {name}"
+
+
+def login(name):
+    return f"Login as {name}"
+
+
+def pause_internet(name):
+    return f"Pause Internet for {name}"
+
+
+def welcome(name):
+    return f"{name}, welcome to Verizon Smart Family"
+
+
+def unexpected_intent(name, intent):
+    return f"{name}, I do not recognize the command {intent}"
+
+
+def no_intent(name):
+    return f"{name}, I did not receive a command"
+
+
+def make_response_dict(response_str):
+    """
+    Return a response dictionary based on an input text to speech string.
+    """
+    return dict(
+        payload=dict(
+            google=dict(
+                expectUserResponse=True,
+                richResponse=dict(
+                    items=[
+                        dict(
+                            simpleResponse=dict(
+                                textToSpeech=response_str
+                            )
+                        )
+                    ]
+                )
+            )
+        )
+    )
+
+
+def error():
+    return "I'm sorry, something went wrong. My bad."
+
+
 def hello(request):
     """Responds to any HTTP request.
     Args:
@@ -32,7 +82,7 @@ def hello(request):
         stav_test(request)
         rich_test(request)
 
-        print("XXX10 request follows")
+        print("XXX11 request follows")
         print(request)
         print("dict follows")
         print(request.__dict__)
@@ -45,49 +95,46 @@ def hello(request):
         print("json follows")
         request_json = request.get_json()
         print(request_json)
-        request_dict = loads(request_json)
-        print("dict follows")
-        print(request_dict)
 
-        # pprint.pprint(request_json)
-        # response_dict = dict(
-        #     conversationToken="conversation_token",
-        #     userStorage="user_storage",
-        #     resetUserStorage=False,
-        #     expectUserResponse=False,
-        #     # expectedInputs=[],
-        #     # finalResponse=None,
-        #     # customPushMessage=None,
-        #     isInSandbox=True,
-        # )
-        tts = "this is a simple response from github"
+        # request_dict = loads(request_json)
+        # print("dict follows")
+        # print(request_dict)
+
+        name = request_json.get('queryResult', {}).get('parameters', {}).get('given-name', None)
+        print(f"name: {name}")
+        # XXX really we probably should punt
+        if not name:
+            name = "No Name"
+
+        intent = request_json.get('queryResult', {}).get('intent', {}).get('displayName', None)
+        print(f"intent: {intent}")
+
+        if (intent == 'Get Location'):
+            response_str = get_location(name)
+        elif (intent == 'Login'):
+            response_str = login(name)
+        elif (intent == 'Pause Internet'):
+            response_str = pause_internet(name)
+        elif (intent == 'Welcome'):
+            response_str = welcome(name)
+        elif intent is not None:
+            # XXX really we probably should punt
+            response_str = unexpected_intent(name)
+        else:
+            # XXX really we probably should punt
+            response_str = no_intent(name)
+
     except Exception as e:
         print("in exception handler")
         print("exception is")
         print(e)
         print("exc info is")
         print(sys.exc_info())
-        tts = "I'm sorry, something went wrong. My bad."
+        response_str = error()
 
-    response_dict = dict(
-        payload=dict(
-            google=dict(
-                expectUserResponse=True,
-                richResponse=dict(
-                    items=[
-                        dict(
-                            simpleResponse=dict(
-                                textToSpeech=tts
-                            )
-                        )
-                    ]
-                )
-            )
-        )
-    )
-
+    response_dict = make_response_dict(response_str)
     response_json = dumps(response_dict)
-    print("XXX10 response follows")
+    print("XXX11 response follows")
     print(response_json)
     # pprint.pprint(response_json)
     return response_json
