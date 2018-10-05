7123867f89ecc393d699dde525ec335717952946
Rich Fromm <rich@locationlabs.com>
2018-10-05 06:51:57 -0700

    set to null string to clear storage

--- main.py
+++ main.py
@@ -302,6 +302,7 @@ def hello(request):
 
         if 'resetUserStorage' in response_dict['payload']['google']:
             print("Resetting user storage")
+            response_dict['payload']['google']['userStorage'] = ""
         else:
             print(f"write user_storage: {user_storage}")
             response_dict['payload']['google']['userStorage'] = dict_to_str(user_storage)
